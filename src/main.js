a6ccea14615f7c588a044070e0ed22b5a935ada2
QingboTian <tqb820965236@163.com>
2020-02-24 11:44:16 +0800

    day1 note

---
+++ src/main.js
@@ -0,0 +1,8 @@
+// 浏览器是无法识别的 需要使用webpack将main.js 解析执行
+import $ from 'jquery'; 
+
+import "./css/index.css"
+
+$(function(){
+    
+})
---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+const path = require('path')
+
+module.exports = {
+    mode: "development",// 开发环境
+    // entry: path.join(__dirname, './src/main.js'),// 输入源
+    entry: {
+        main : './src/main.js'
+    },
+    output: {// 输出源
+        // path : path.join(__dirname, './dist'), // 输出路径
+        path : path.resolve(__dirname, './dist'),
+        filename : 'bundle.js'// 输出文件
+    },
+    devServer : {
+        open : true,
+        port : 8888,
+        contentBase : 'src',
+        hot : true
+    },
+    // 配置第三方加载器 如css
+    module:{
+        rules : [
+            {
+                test : /\.css$/,// 正则匹配导入文件后缀名
+                use : [// 使用什么加载器
+                    {
+                        loader: 'style-loader',
+                    },
+                    {
+                        loader: 'css-loader',
+                        options : {
+                            // url : true
+                        }
+                    }
+                ]
+            },
+            // {
+            //     test : /\.less$/,// 正则匹配导入文件后缀名
+            //     use : [// 使用什么加载器
+            //         {
+            //             loader: 'style-loader',
+            //         },
+            //         {
+            //             loader: 'css-loader',
+            //         },
+            //         {
+            //             loader: 'less-loader',
+            //         }
+            //     ]
+            // }
+            {
+                test : /\.(jpg|png|gif|bmp|jpeg)$/,// 正则匹配导入文件后缀名
+                use : [// 使用什么加载器
+                    {
+                        loader: 'url-loader',
+                        options : {
+                            limit : 7631// KB 低于这个大小的图片会采用Base64编码展示 大于则采用URL
+                        }
+                    },
+                ]
+            },
+            {
+                test : /\.(ttf|eot|svg|woff|woff2)$/,// 正则匹配导入文件后缀名
+                use : [// 使用什么加载器
+                    {
+                        loader: 'url-loader',
+                    },
+                ]
+            },
+        ]
+    }
+}
